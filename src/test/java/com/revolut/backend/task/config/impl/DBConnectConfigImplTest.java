53bd467b5c5fed1f4b89b0e809b84a36339b3584
Sana Parveen <sana_parveen@epam.com>
2019-11-11 18:59:32 +0530

    added test cases

--- src/test/java/com/revolut/backend/task/config/impl/DBConnectConfigImplTest.java
+++ src/test/java/com/revolut/backend/task/config/impl/DBConnectConfigImplTest.java
@@ -8,12 +8,10 @@ import java.sql.SQLException;
 import org.h2.tools.RunScript;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import com.revolut.backend.task.config.DBConnectConfig;
 
